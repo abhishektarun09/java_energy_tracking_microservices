a98e2baeacc1840ec39743c3a300de6244591e6d
Abhishek Tarun <abhishek.tarun09@gmail.com>
2026-07-30 16:24:34 +0200

    added ingestion-service service to send iot data to kafka topic

--- ingestion-service/src/main/java/com/atarun/ingestion_service/service/IngestionService.java
+++ ingestion-service/src/main/java/com/atarun/ingestion_service/service/IngestionService.java
@@ -1,8 +1,32 @@
 package com.atarun.ingestion_service.service;
 
+import com.atarun.ingestion_service.dto.EnergyUsageDTO;
+import com.atarun.ingestion_service.kafka.event.EnergyUsageEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class IngestionService {
+
+    private final KafkaTemplate<String, EnergyUsageEvent> kafkaTemplate;
+
+    public IngestionService(KafkaTemplate<String, EnergyUsageEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void ingestEnergyUsage(EnergyUsageDTO input) {
+        // Convert DTO to Event
+        EnergyUsageEvent event = EnergyUsageEvent.builder()
+                .deviceId(input.deviceId())
+                .energyConsumed(input.energyConsumed())
+                .timestamp(input.timestamp())
+                .build();
+
+        // Send to Kafka Topic
+        kafkaTemplate.send("energy-usage", event);
+        log.info("Ingested Energy Usage Event: {}", event);
+    }
 
 }
